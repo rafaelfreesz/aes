58f6ae97db1bd3ddb81e9ce1ba52e0dd7dda696b
Rafael Freesz <rafaelfreesz@gmail.com>
2024-04-13 21:39:58 -0300

    Primeiro commit

---
+++ Lista 1 -/Listas/Lista.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rafael on 13/04/2024.
+//
+
+#include "Lista.h"
---
+++ Lista 1 -/Listas/Lista.h
@@ -0,0 +1,23 @@
+//
+// Created by Rafael on 13/04/2024.
+//
+
+#ifndef LISTA_H
+#define LISTA_H
+
+
+class Lista {
+
+    
+
+};
+
+class No {
+    public:
+        int valor;
+        No* proximo;
+        No* anterior;
+};
+
+
+#endif //LISTA_H
